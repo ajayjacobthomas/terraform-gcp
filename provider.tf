b71ead47fe71f8b165cdb72324399b479cddeadd
Ajay Thomas <ajayjacobthomas@gmail.com>
2024-04-04 13:11:40 +0100

    New commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("terraform-kickstart-ac4a9e1b283d.json")
   project     = "terraform-kickstart"
   region      = "europe-west4"
   zone        = "europe-west4-a"
